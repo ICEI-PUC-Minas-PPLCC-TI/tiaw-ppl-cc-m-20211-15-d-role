d413a2b2c19497a4ba8067832364327b9ddd3f5d
Bruno Faria <bruno.fariadevl@gmail.com>
2021-05-22 19:45:04 -0300

    V1.0.3 - Adição Modal/Armazenamento dados
    Modificações Feitas:
    - Modal adicionado na página que pode ser modificada
    -  Modificações no Perfil que pode ser modificado (Colocar coisas anônimas para o usuário modificar)
    - Adicionado editar.js, já salvando dados no localStorage para adicionar as modificações ao perfil

---
+++ scripts/editar.js
@@ -0,0 +1,34 @@
+window.onload = function() {
+    var btnSalvar = document.getElementById("salvaalt");
+
+    btnSalvar.onclick = salvarDados;
+}
+
+function salvarDados() {
+    var inputNome = document.getElementById("NAME");
+    var inputSobre = document.getElementById("sobre");
+    var inputTipo = document.getElementById("tipo")
+    var inputLocalizacao = document.getElementById("local");
+
+    var Name = inputNome.value;
+    var About = inputSobre.value;
+    var Local = inputLocalizacao.value;
+    var Tipo = inputTipo.value;
+
+    localStorage.setItem("NOMESALVO", Name);
+    localStorage.setItem("SOBRESALVO", About);
+    localStorage.setItem("LOCALSALVO", Local);
+    localStorage.setItem("TIPOSALVO", Tipo);
+
+    /*
+    document.getElementById("mudaNome").innerText = ;
+    document.getElementById("mudaSobre").innerText = ;
+    document.getElementById("mudaLocal").innerText = ;
+    if('tipo' == 1) {
+        document.getElementById("mudaTipo").innerText = "Estabelecimento";
+    } else {
+        document.getElementById("mudaTipo").innerText = "Músico";
+    }
+     */
+
+}
