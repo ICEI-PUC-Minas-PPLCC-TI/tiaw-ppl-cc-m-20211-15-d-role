31349177240170288dc4fe9ae8db2a1691c93a90
Bruno Faria <bruno.fariadevl@gmail.com>
2021-05-23 23:07:24 -0300

    V1.0.4 - localStorage Modal Editar perfil e modificações no profiledono.html
    Modificações no profile dono para interagir com JavaScript
    Falta tratar o localStorage como array para as modificações serem aceitas no perfil

--- scripts/editar.js
+++ scripts/editar.js
@@ -1,38 +1,52 @@
-window.onload = function() {
-    var btnSalvar = document.getElementById("salvaalt");
-
-    btnSalvar.onclick = salvarDados;
-}
-
-function salvarDados() {
-    var inputNome = document.getElementById("NAME");
-    var inputSobre = document.getElementById("sobre");
-    var inputTipo = document.getElementById("tipo")
-    var inputLocalizacao = document.getElementById("local");
-
-    var Name = inputNome.value;
-    var About = inputSobre.value;
-    var Local = inputLocalizacao.value;
-    var Tipo = inputTipo.value;
-
-    localStorage.setItem("NOMESALVO", Name);
-    localStorage.setItem("SOBRESALVO", About);
-    localStorage.setItem("LOCALSALVO", Local);
-    localStorage.setItem("TIPOSALVO", Tipo);
-
-    /*
-    document.getElementById("mudaNome").innerText = ;
-    document.getElementById("mudaSobre").innerText = ;
-    document.getElementById("mudaLocal").innerText = ;
-    if('tipo' == 1) {
-        document.getElementById("mudaTipo").innerText = "Estabelecimento";
-    } else {
-        document.getElementById("mudaTipo").innerText = "Músico";
-    }
-     */
-
-}
+window.onload = loadPage;
 
 function alertaAdicionarFoto() {
-    alert("Funcionalidade indisponível no momento.")
+    alert("Funcionalidade indisponível no momento, nenhum arquivo ficará salvo no perfil.")
 }
+
+var db_modificacoes = {};
+
+function addModificacoes(nome, tipo, sobre, local) {
+    let modificacoes = {
+        "Nome": nome,
+        "Tipo": tipo,
+        "Sobre": sobre,
+        "Local": local
+    };
+    localStorage.setItem('db_modificacoes', JSON.stringify(modificacoes));
+}
+
+function loadPage() {
+    let mudarNomeAndTipo = document.getElementById('mudaNomeTipo');
+    let mudarSobre = document.getElementById('mudaSobre');
+    let mudarLocal = document.getElementById('mudaLocal');
+
+    let changeNameAndTipo = `<h4 class="mt-0 mb-0">${db_modificacoes.nome}</h4>
+                                 <p class="small mb-4 tipo">${db_modificacoes.tipo}</p>`
+    let changeSobre = `<div class="p-4 rounded shadow-sm bg-light">
+                              <p>${db_modificacoes.sobre}</p> 
+                            </div>`
+    let changeLocal = `<p>${db_modificacoes.local}</p>`
+
+    mudarNomeAndTipo.innerHTML = changeNameAndTipo;
+    mudarSobre.innerHTML = changeSobre;
+    mudarLocal.innerHTML = changeLocal;
+}
+
+
+function salvaDados(event) {
+    event.preventDefault();
+
+    let nome = document.getElementById('NAME').value;
+    let tipo = document.getElementById('tipo').value;
+    let sobre = document.getElementById('sobre').value;
+    let local = document.getElementById('local').value;
+
+    addModificacoes(nome, tipo, sobre, local);
+
+    alert('Dados salvos com sucesso.')
+    window.location = "profiledono.html";
+}
+
+// Associar salvamento ao botao
+document.getElementById('salvaalt').addEventListener('click', salvaDados);
